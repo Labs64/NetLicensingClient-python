e62f81c28c67a9273cd026a43ca6e9f8f8cd1f17
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-06-13 09:53:44 +0200

    - revisit classifiers

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(name='netlicensing-python-client',
                  version='0.0.1',
                  description='Python wrapper for Labs64 NetLicensing RESTful API',
                  long_description=open('README.md').read().strip(),
+                 long_description_content_type="text/markdown",
                  author='Labs64 NetLicensing',
                  author_email='info@netlicensing.io',
                  url='https://github.com/Labs64/NetLicensingClient-python',
@@ -12,4 +13,18 @@ setuptools.setup(name='netlicensing-python-client',
                  license='Apache-2.0',
                  zip_safe=False,
                  keywords='labs64 netlicensing licensing licensing-as-a-service license license-management software-license client restful restful-api python wrapper api client',
-                 classifiers=['Package', 'Wrapper', 'Client', 'API'])
+                 classifiers=[
+                     # https://pypi.org/classifiers/
+                     'Development Status :: 1 - Planning',
+                     'Environment :: Other Environment',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Information Technology',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3',
+                     'Topic :: Software Development :: Libraries :: Python Modules',
+                     'Topic :: System :: Software Distribution',
+                     'Topic :: Utilities'
+                 ],
+                 python_requires='>=3.6',
+                 )
