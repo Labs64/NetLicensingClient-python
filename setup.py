7fdbfe733200b4d820d0dd7fcd16a384da4c93e0
R.Brown <ready.brown@hotmail.de>
2020-06-15 07:06:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/#setup-args
 setup(name='netlicensing-client',
-                 version='0.0.5',
+                 version='0.0.6',
                  description='Python wrapper for Labs64 NetLicensing RESTful API',
                  long_description=open('README.md').read().strip(),
                  long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setup(name='netlicensing-client',
                  packages=find_packages(),
                  license='Apache-2.0',
                  zip_safe=False,
-                 keywords='labs64 netlicensing licensing licensing-as-a-service license license-management software-license client restful restful-api python wrapper api client',
+                 keywords='labs64 netlicensing software licensing Licensing-as-a-Service LaaS license license-management software-license client restful restful-api python wrapper api client licensing library',
                  classifiers=[
                      # https://pypi.org/classifiers/
                      'Development Status :: 1 - Planning',
@@ -38,6 +38,7 @@ setup(name='netlicensing-client',
                      'Programming Language :: Python :: 3.6',
                      'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8',
+                     'Topic :: Software Development :: Build Tools',
                      'Topic :: Software Development :: Libraries :: Python Modules',
                      'Topic :: System :: Software Distribution',
                      'Topic :: Utilities'
